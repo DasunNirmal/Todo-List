9df3223a9e1355100846b539337d7e8e39855790
DasunNirmal <dasun200301@gmail.com>
2025-04-09 00:01:17 +0530

    light and dark theme toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-taskmanager';
+  isLightTheme = false;  /*Tracks whether the theme is light or dark*/
+
+  toggleTheme() {
+    this.isLightTheme = !this.isLightTheme;
+    document.body.classList.toggle('light', this.isLightTheme);
+  }
 }
