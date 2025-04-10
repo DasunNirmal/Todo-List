9ef051ba00354a7222af4b3750dd0c3ff8ed9c1b
DasunNirmal <dasun200301@gmail.com>
2025-04-10 22:16:31 +0530

    add tasks done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,22 +1,23 @@
 import {Component, OnInit} from '@angular/core';
-import {NgClass, NgFor} from '@angular/common';
+import {CommonModule, DatePipe, NgClass, NgFor} from '@angular/common';
 import {TaskService} from '../../services/task.service';
 import {Tasks} from '../../model/tasks/tasks.module';
 
 @Component({
   selector: 'app-home',
-  imports: [
-    NgClass,
-    NgFor
-  ],
+  standalone: true,
+  imports: [NgClass, NgFor, CommonModule],
+  providers: [DatePipe],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   isLightTheme = false;  /*Tracks whether the theme is light or dark*/
   tasks: Tasks[] = [];
+  currentDate: string | null = '';
+  currentStatus: string  = 'PENDING';
 
-  constructor(private taskService: TaskService) {}
+  constructor(private taskService: TaskService, private date: DatePipe) {}
 
   toggleTheme() {
     this.isLightTheme = !this.isLightTheme;
@@ -24,9 +25,11 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.currentDate = this.date.transform(new Date(), 'yyyy-MM-dd');
     this.loadAllTasks();
   }
 
+  /*CRUD*/
   loadAllTasks() {
     this.taskService.getAllTasks().subscribe((data: any) => {
       this.tasks = data;
@@ -36,5 +39,31 @@ export class HomeComponent implements OnInit {
 
   trackTaskById(index: number, task: Tasks): string {
     return task.id;
+  }
+
+  addTask(value: string) {
+    if (!value.trim()) return;
+
+    const newTask: Tasks = {
+      id: '', /*back end will handle the id*/
+      description: value,
+      status: this.currentStatus,
+      createdAt: this.currentDate || ''
+    };
+
+    this.taskService.addTask(newTask).subscribe({
+      next: () => {
+        this.loadAllTasks(); // Refresh the task list
+      },
+      error: (error) => {
+        console.error('Error adding task:', error);
+      }
+    });
+  }
+
+  changeStatus(task: Tasks, event: Event) {
+    const target = event.target as HTMLInputElement;
+    task.status = target.checked ? 'COMPLETED' : 'PENDING';
+    console.log(task);
   }
 }
