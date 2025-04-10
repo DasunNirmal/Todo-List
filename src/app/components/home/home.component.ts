c5826af491ad92cade5697b84456603312c7afc7
DasunNirmal <dasun200301@gmail.com>
2025-04-10 13:41:57 +0530

    get all tasks done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgClass} from '@angular/common';
+import {TaskService} from '../../services/task.service';
+import {Tasks} from '../../model/tasks/tasks.module';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +11,24 @@ import {NgClass} from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   isLightTheme = false;  /*Tracks whether the theme is light or dark*/
+  tasks: Tasks[] = [];
+
+  constructor(private taskService: TaskService) {}
 
   toggleTheme() {
     this.isLightTheme = !this.isLightTheme;
     document.body.classList.toggle('light', this.isLightTheme);
+  }
+
+  ngOnInit() {
+    this.loadAllTasks();
+  }
+
+  loadAllTasks() {
+    this.taskService.getAllTasks().subscribe((data) => {
+      console.log(data);
+    })
   }
 }
