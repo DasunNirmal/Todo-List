ce0f88b28864a6d25de39d9d2d9ced86fabaacec
DasunNirmal <dasun200301@gmail.com>
2025-04-11 18:03:59 +0530

    navigation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  constructor(private router: Router) { }
 
+  login() {
+    this.router.navigate(['home']).then(r => true);
+  }
+
+  goToRegister() {
+    this.router.navigate(['register']).then(r => true);
+  }
 }
